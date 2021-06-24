4adbd8e61029d3cc1d048d07d0f62e1bc76d897a
Lazir Pascual <lazirpascual@gmail.com>
2021-06-24 17:45:22 -0400

    add styling for task card

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -6,13 +6,33 @@ import IconButton from "@material-ui/core/IconButton";
 import DeleteOutlined from "@material-ui/icons/DeleteOutlined";
 import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
+import FolderIcon from "@material-ui/icons/Folder";
+import { purple } from "@material-ui/core/colors";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  purple: {
+    color: theme.palette.getContrastText(purple[500]),
+    backgroundColor: purple[200],
+  },
+  description: {
+    paddingLeft: theme.spacing(3),
+  },
+}));
 
 export default function TaskCard(props) {
+  const classes = useStyles();
   const { task, deleteTask, updateTask, updateDescription } = props;
   return (
     <div>
       <Card variant="Media">
         <CardHeader
+          avatar={
+            <Avatar className={classes.purple}>
+              <FolderIcon />
+            </Avatar>
+          }
           action={
             <IconButton onClick={() => deleteTask(task.id)}>
               <DeleteOutlined />
@@ -25,7 +45,7 @@ export default function TaskCard(props) {
               onChange={(e) => updateTask(e.target.value, task.id)}
               key={task.id}
               type="text"
-              variant="outlined"
+              variant="standard"
               color="secondary"
             ></TextField>
           }
@@ -34,14 +54,15 @@ export default function TaskCard(props) {
           <Typography variant="body2" color="textSecondary">
             {
               <TextField
-                value={task.description}
                 label="Description"
+                value={task.description}
                 onChange={(e) => updateDescription(e.target.value, task.id)}
                 key={task.id}
                 type="text"
-                variant="outlined"
+                variant="filled"
                 color="secondary"
                 multiline
+                fullWidth
                 rows={4}
               ></TextField>
             }
